2826185d084c80a2cc8a9c748895e03c66b1fa06
Be30nCH1k <danildudin592@gmail.com>
2024-12-05 15:23:51 +0300

    change

--- sight/assets/JS/script.js
+++ sight/assets/JS/script.js
@@ -1,31 +1,51 @@
 const searchInput = document.getElementById('searchInput');
+
 const container = document.getElementById('data-list');
+
 const mochApi = 'https://6729bdac6d5fa4901b6e27f4.mockapi.io/attractions';
+
+const categoryFilter = document.getElementById('categoryFilter');
+
+const sortName = document.getElementById("sortName")
 
 const closeBtn = document.querySelector('.close-btn');
 const socialPanelContainer = document.querySelector('.social-panel-container');
 const modalSocial = document.querySelector('.obratnuj__zvonok');
 
-const itemOnPage = 5;
+const itemOnPage = 10;
 let currentPage = 1;
 let persone = [];
 let filteredPersone = [];
 let totalItems;
 let totalPages;
 
-
-
-
 addEventListener('load', async () => {
-    const users = await fetch(mochApi);
-    persone = await users.json();
+    showLoader();
+    const response = await fetch(mochApi);
+    persone = await response.json();
     filteredPersone = [...persone];
     totalItems = filteredPersone.length;
     totalPages = Math.ceil(totalItems / itemOnPage);
 
-    loadDataInMochApi(currentPage);
-    search();
+ // Беру параметр category и добавляю в html categoryFilter уникальные районы
+    const uniquecategory = [...new Set(persone.map(person => person.category))];
+    populatecategoryFilter(uniquecategory);
+
+ hideLoader();
+ loadDataInMochApi(currentPage);
+ search();
 });
+
+function populatecategoryFilter(categorys) {
+ // Очистка текущий функции
+    categoryFilter.innerHTML = '<option value="">Фильтр по категориям</option>';
+        categorys.forEach(category => {
+        const option = document.createElement('option');
+        option.value = category; // Значение для фильтрации
+        option.textContent = category; // Отображаемый текст
+        categoryFilter.appendChild(option);
+    });
+}
 
 closeBtn.addEventListener('click', () => {
     socialPanelContainer.classList.remove('visible');
@@ -39,17 +59,12 @@ function hideLoader() {
     document.getElementById('loader').style.display = 'none';
 }
 
-function loadData() {
-    showLoader();
-}
-
 function loadDataInMochApi(item) {
     showLoader(); 
-
     container.innerHTML = '';
     const start = (item - 1) * itemOnPage;
     const end = Math.min(start + itemOnPage, totalItems);
-
+ 
     for (let i = start; i < end; i++) {
         const t = filteredPersone[i];
         const search = document.createElement('div');
@@ -57,44 +72,44 @@ function loadDataInMochApi(item) {
         search.setAttribute('data-title', t.name);
         search.innerHTML = `
             <img class="info__img" src="${t.img}" alt="картинки ${i + 1}">
-            <div class="info__box">
+                <div class="info__box">
                 <div class="info__title">${t.name}</div>
                 <button class="info__btn floating-btn" id="btn_${i}">Увидеть больше</button>
             </div>`;
-        
+ 
         container.append(search);
-
+        
         let infoBtn = document.getElementById(`btn_${i}`);
         infoBtn.addEventListener('click', () => {
-            socialPanelContainer.classList.add('visible');
-            modalSocial.innerHTML = `
-                <div class="modal__title">${t.name}</div>
-                <div class="modal__box">
-                    <div class="modal__text">${t.title}</div>
-                    <img class="modal__img" src="${t.img}" alt="${t.name}">
-                </div>
-                <div id="map"></div>`;
-            
-            ymaps.ready(() => {
-                const map = new ymaps.Map('map', {
-                    center: [t.lat, t.lng],
-                    zoom: 17,
+        socialPanelContainer.classList.add('visible');
+        modalSocial.innerHTML = `
+        <div class="modal__title">${t.name}</div>
+            <div class="modal__box">
+                <div class="modal__text">${t.title}</div>
+                <img class="modal__img" src="${t.img}">
+            </div>
+        <div id="map"></div>`;
+        
+        ymaps.ready(() => {
+            const map = new ymaps.Map('map', {
+                center: [t.lat, t.lng],
+                zoom: 17,
                 });
-                const placemark = new ymaps.Placemark([t.lat, t.lng]);
-                map.geoObjects.add(placemark);
+                    const placemark = new ymaps.Placemark([t.lat, t.lng]);
+                    map.geoObjects.add(placemark);
+                });
             });
-        });
-    }
+        }
 
-    updatePag();
+    updatePagination();
     hideLoader();
 }
 
-function updatePag() {
+function updatePagination() {
     const paginationContainer = document.getElementById('pagination');
     paginationContainer.innerHTML = '';
 
-    for (let i = 1; i <= totalPages; i++) {
+ for (let i = 1; i <= totalPages; i++) {
         const pageBtn = document.createElement('button');
         pageBtn.textContent = i;
         pageBtn.onclick = () => changePage(i);
@@ -103,22 +118,46 @@ function updatePag() {
 }
 
 function changePage(page) {
-    currentPage = page; loadDataInMochApi(currentPage); 
-}
-
-function search() {
-    searchInput.addEventListener('input', function() {
-        const i = this.value.toLowerCase();
-        filteredPersone = persone.filter(section => 
-            section.name.toLowerCase().includes(i)
+    currentPage = page; 
+    loadDataInMochApi(currentPage); 
+   }
+   
+async function search() {
+    searchInput.addEventListener('input', async function() {
+    const query = this.value.trim().toLowerCase();
+    const category = categoryFilter.value; // Получаем выбранный категорию
+   
+    // Это фильтр для поиска
+    filteredPersone = persone.filter(person => 
+        person.name.toLowerCase().includes(query) && 
+            (category === '' || person.category === category) // Фильтрация по категории
         );
-        totalItems = filteredPersone.length; 
-        totalPages = Math.ceil(totalItems / itemOnPage); 
-        currentPage = 1; 
-
+   
+    totalItems = filteredPersone.length; 
+    totalPages = Math.ceil(totalItems / itemOnPage); 
+    currentPage = 1; 
+   
         loadDataInMochApi(currentPage); 
-    });
-}
+        });
+    }
+   
+categoryFilter.addEventListener('change', async function() {
+    const query = searchInput.value.trim().toLowerCase();
+    const category = this.value;
+   
+    // А это основной фильтр
+    filteredPersone = persone.filter(person => 
+    person.name.toLowerCase().includes(query) && 
+        (category === '' || person.category === category) // Фильтрация по категории
+    );
+   
+    totalItems = filteredPersone.length; 
+    totalPages = Math.ceil(totalItems / itemOnPage); 
+    currentPage = 1; 
+   
+    loadDataInMochApi(currentPage); 
+});
+   
 
 function sortData(order) {
     if (order === 'abc') {
@@ -131,6 +170,3 @@ function sortData(order) {
 document.getElementById('sortName').addEventListener('click', () => {
     sortData('abc');
 });
-
-loadData();
-
